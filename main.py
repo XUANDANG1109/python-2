89091fb62941f4be9c30f62f189113f47a0b1e60
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-08-29 16:44:57 +0300

    aa

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ print("Hello " + name + "!")
 print("How it is with you, " + name + "!")
 # Phase 1
 name = input("what is your name?")
-print("Hello, "+name+"!")
+print("Hello, " + name + "!")
 
 # Phase 2
 radius = float(input("Give the radius?"))
-area = math.pi * radius **2
+area = math.pi * radius ** 2
 print("Area is " + str(area))
 print(f"Area is {area:.1f}")
-print(f"Area is {math.pi* radius **2:.1f}")
+print(f"Area is {math.pi * radius ** 2:.1f}")
 
 # phase 6
-print(f"{random.randint(0,999):03d}")
-print(str(random.randint(0,9))+ str(random.randint(0,9)) + str(random.randint(0,9)))
+print(f"{random.randint(0, 999):03d}")
+print(str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)))
 
 # phase 3
 length = float(input("Give the length"))
 width = float(input("Give the width"))
-perimeter = (length + width) *2
+perimeter = (length + width) * 2
 area = length * width
 print(f"perimeter is {perimeter: .1f}")
 print(f"area is {area: .1f}")
@@ -33,10 +33,20 @@ numberone = int(input("Give the integer number 1"))
 numbertwo = int(input("Give the integer number 2"))
 numberthree = int(input("Give the integer number 3"))
 sum = numberone + numbertwo + numberthree
-product = numberone*numbertwo*numberthree
-average = sum/3
+product = numberone * numbertwo * numberthree
+average = sum / 3
 print(sum)
 print(product)
 print(f"average is {average:.1f}")
 
 # Phase 5
+convert_talent = float(input("Enter talent to convert into kilograms:"))
+convert_pounds = float(input("Enter pounds to convert into kilograms:"))
+convert_lot = float(input("Enter lot to convert into kilograms:"))
+talent = 20 * convert_talent * 32 * 13.3
+pounds = 32 * convert_pounds *13.3
+lot = 13.3 * convert_lot
+sum_gram = talent + pounds + lot
+result_kg = int(sum_gram // 1000)
+result_gram = float(sum_gram % 1000)
+print("The weight in modern units:",result_kg, f"kilograms and, {result_gram:.2f} grams")
